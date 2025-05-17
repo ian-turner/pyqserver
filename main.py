7bfe48e05e9d3ea822837e91006ef817aa71474d
ian-turner <i.r.turner55@gmail.com>
2025-05-17 16:09:34 -0400

    added argument parsing

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import socket
 from multiprocessing import Process
+from argparse import ArgumentParser
 
 
 # server config
 HOST = '127.0.0.1'
-PORT = 1901
-NUM_WORKERS = 20
+DEFAULT_PORT = 1901
+DEFAULT_NUM_WORKERS = 30
 
 
 def socket_worker(socket):
@@ -28,14 +29,21 @@ def socket_worker(socket):
 
 
 if __name__ == '__main__':
+    # reading command line arguments
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', type=int, default=DEFAULT_PORT)
+    parser.add_argument('-n', '--num_workers', type=int, default=DEFAULT_NUM_WORKERS)
+    args = parser.parse_args()
+
     # setting up socket connection
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        print('Starting quantum server')
-        s.bind((HOST, PORT))
+        print('Starting quantum server on port %d with %d workers' % (args.port, args.num_workers))
+        s.bind((HOST, args.port))
         s.listen()
         
         # starting worker processes to listen for connections
-        procs = [Process(target=socket_worker, args=(s,)) for _ in range(NUM_WORKERS)]
+        procs = [Process(target=socket_worker, args=(s,)) \
+                 for _ in range(args.num_workers)]
         for p in procs:
             p.start()
 
