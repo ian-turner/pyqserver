0a8b4d0ffc65003d98f7d8784b51fed97244a220
ian-turner <i.r.turner55@gmail.com>
2025-05-28 17:44:47 -0400

    more setup for testing

--- test/test_server.py
+++ test/test_server.py
@@ -8,12 +8,19 @@ SERVER_PORT = 1901
 def client():
     """Set up a test socket client"""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect(('127.0.0.1', SERVER_PORT))
         yield client
+        client.close()
 
 
-@pytest.mark.order(1)
 def test_connection(client):
     """Testing connection to qserver"""
-    client.connect(('127.0.0.1', SERVER_PORT))
     client.send(b'Universal\nquit\n')
-    client.close()
+
+
+def test_init_commands(client):
+    """Testing basic bit/qubit initialization operations"""
+    with open('test/inits.txt', 'r') as f:
+        lines = [x.strip() + '\n' for x in f.readlines()]
+        for line in lines:
+            client.send(line.encode())
